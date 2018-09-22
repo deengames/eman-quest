3261caf24e2b91dcc5b43673828c4252c65fe447
nightblade9 <nightbladecodes@gmail.com>
2018-09-22 19:28:50 -0400

    Inventory shows current weapon/armour and inventory

--- source/Scenes/UI/InventoryWindow.gd
+++ source/Scenes/UI/InventoryWindow.gd
@@ -5,12 +5,18 @@ extends WindowDialog
 # var b = "textvar"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	self._update_equipped_display()
 	$Equipment/SelectedWeapon.text = ""
 	$Equipment/SelectedArmour.text = ""
+	self._populate_item_list()
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _update_equipped_display():
+	$Equipment/CurrentWeapon.text = Globals.player_data.weapon.str()
+	$Equipment/CurrentArmour.text = Globals.player_data.armour.str()
+
+func _populate_item_list():
+	$ItemList.add_item(Globals.player_data.weapon.equipment_name)
+	$ItemList.add_item(Globals.player_data.armour.equipment_name)
+	
+	for item in Globals.player_data.inventory:
+		$ItemList.add_item(item.equipment_name)
