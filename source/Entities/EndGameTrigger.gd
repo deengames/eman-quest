b9455248a4f788e534336f463cd0703932da5e61
nightblade9 <nightbladecodes@gmail.com>
2018-09-28 19:53:02 -0400

    End-game NPC works!

--- source/Entities/EndGameTrigger.gd
+++ source/Entities/EndGameTrigger.gd
@@ -1,8 +1,14 @@
-extends StaticBody2D
+extends Area2D
+
+const StoryWindow = preload("res://Scenes/UI/StoryWindow.tscn")
 
 func _on_Area2D_body_entered(body):
 	if body == Globals.player:
+		body.free()
 		self._show_endgame_story()
 
 func _show_endgame_story():
-	pass
+	var story_window = StoryWindow.instance()
+	story_window.show_ending_story()
+	get_tree().get_root().add_child(story_window)
+	story_window.popup_centered()
--- source/Scenes/UI/StoryWindow.gd
+++ source/Scenes/UI/StoryWindow.gd
@@ -11,13 +11,29 @@ func show_intro_story():
 	$Contents.text = "A young boy from the village of " + village_name + " wakes up one day to find an enourmous " + final_boss_type + " creature emerge and kidnap the village imam. The creature flees toward the forest."
 	self._on_close_callback = funcref(self, "_change_to_overworld_map")
 
+func show_ending_story():
+	self.window_title = "An Adventure Ends!"
+	var village_name = Globals.story_data["village_name"]
+	var final_boss_type = Globals.story_data["boss_type"]
+	
+	var verb = "hissing"
+	if final_boss_type.find("dog") > -1:
+		verb = "growling"
+		
+	$Contents.text = "The boy charges the " + final_boss_type + ", which flees into the distance, " + verb + ".\n\nThe imam, shaken, bleeding slightly, staggers to his feet and thanks the boy; together, they return to the village.\n\nTHE END!"
+	self._on_close_callback = funcref(self, "_change_to_main_menu")
+
 func _change_to_overworld_map():
 	SceneManagement.change_map_to(get_tree(), "Overworld")
+
+func _change_to_main_menu():
+	get_tree().change_scene("res://Scenes/Title.tscn")
 
 func _on_Node2D_popup_hide():
 	if self._on_close_callback != null:
 		self._on_close_callback.call_func()
 		self._on_close_callback = null
-
+	self.queue_free()
+	
 func _on_CloseButton_pressed():
 	self.emit_signal("popup_hide")
