3e5ec8cf4ff4e5a32b9d4b03d62f56307ec62077
nightblade9 <nightbladecodes@gmail.com>
2018-11-02 23:17:23 -0400

    Mouse click coordinates seem off. Manually fix them.

--- source/Scenes/PopulatedMapScene.gd
+++ source/Scenes/PopulatedMapScene.gd
@@ -194,4 +194,7 @@ func _load_tileset_or_auto_tileset(tileset_path):
 func _unhandled_input(event):
 	if (event is InputEventMouseButton and event.pressed) or (OS.has_feature("Android") and event is InputEventMouseMotion):
 		var position = get_global_mouse_position()
+		# Clicks seem ... off for some reason. Not sure why. Adjust manually.
+		position.x -= Globals.TILE_WIDTH / 2
+		position.y -= Globals.TILE_HEIGHT
 		Globals.emit_signal("clicked_on_map", position)
