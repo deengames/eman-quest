472cdc6a355457b16634485e5cc64d554749bd0d
nightblade9 <nightbladecodes@gmail.com>
2018-11-13 17:53:25 -0500

    Fast-paced battle: display target tiles on battle start

--- source/Scenes/Battle/FastPacedMemoryBattleScene.gd
+++ source/Scenes/Battle/FastPacedMemoryBattleScene.gd
@@ -3,11 +3,17 @@ extends Node2D
 var _player # BattlePlayer.new
 var _monster_data = {}
 
+# TODO: as difficulty increases, increase number of tiles to find, and how
+# spread out they are from each other.
+var difficulty = 1
+var _is_players_turn = false
+
 func set_combatants(player, monster_data):
 	self._player = player
 	self._monster_data = monster_data
 	self._monster_data["max_health"] = self._monster_data["health"]
 
+func _ready():
 	var image_name = self._monster_data["type"].replace(' ', '')
 	$MonsterControls/MonsterHealth.max_value = self._monster_data["max_health"]
 	$MonsterControls/MonsterSprite.texture = load("res://assets/images/monsters/" + image_name + ".png")
@@ -15,7 +21,15 @@ func set_combatants(player, monster_data):
 	
 	self._update_health_displays()
 	$StatusLabel.text = ""
-	
+
+	self._show_next_turn()
+
+func _show_next_turn():
+	self._is_players_turn = not self._is_players_turn
+	var num_tiles = 7 # typically, memory is 7 +- 2 items
+	var tiles = $RecallGrid.pick_tiles(difficulty)
+	$RecallGrid.show_tiles(tiles)
+		
 # health and energy
 func _update_health_displays():
 	var player_health = self._player.current_health
--- source/Scenes/Battle/RecallGrid.gd
+++ source/Scenes/Battle/RecallGrid.gd
@@ -18,9 +18,22 @@ func _ready():
 		for x in range(_WIDTH_IN_TILES):
 			var tile = RecallTile.instance()
 			tile.position = Vector2(x * _TILE_WIDTH, y * _TILE_HEIGHT)
+			tile.name = "Tile" + str(x) + "-" + str(y)
 			self.add_child(tile)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func pick_tiles(difficulty):
+	var tiles_left = 7
+	var tiles = []
+	
+	while tiles_left > 0:
+		var next_tile = Vector2(randi() % _WIDTH_IN_TILES, randi() % _HEIGHT_IN_TILES)
+		if not next_tile in tiles:
+			tiles.append(next_tile)
+			tiles_left -= 1
+	
+	return tiles
+
+func show_tiles(tiles):
+	for tile in tiles:
+		var sprite = self.get_node("Tile" + str(tile.x) + "-" + str(tile.y))
+		sprite.region_rect.position.x = _TILE_IMAGE_X["active"]
