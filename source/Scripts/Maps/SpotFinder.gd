d13bba136b7df6f7bf8e1212d242a4a849334de9
nightblade9 <nightbladecodes@gmail.com>
2018-11-25 23:31:04 -0500

    Remove unncessary debug prints

--- source/Scripts/Maps/SpotFinder.gd
+++ source/Scripts/Maps/SpotFinder.gd
@@ -8,7 +8,6 @@ const _ISOLATION_DISTANCE_TILES = 10 # n-tile radius
 static func find_empty_spot(map_width, map_height, ground_map, object_map, walkable_tiles, occupied_spots):
 	var x = Globals.randint(_PADDING_FROM_MAP_EDGES, map_width - _PADDING_FROM_MAP_EDGES - 1)
 	var y = Globals.randint(_PADDING_FROM_MAP_EDGES, map_height - _PADDING_FROM_MAP_EDGES - 1)
-	var debug = null
 	
 	while (object_map.get(x, y) != null or
 		occupied_spots.find([x, y]) > -1 or
@@ -22,16 +21,6 @@ static func find_empty_spot(map_width, map_height, ground_map, object_map, walka
 		walkable_tiles.find(ground_map.get(x, y)) == -1):
 			x = Globals.randint(0, map_width - 1)
 			y = Globals.randint(0, map_height - 1)
-			
-			debug = ground_map.get(x, y)
-			print("D="+str(debug) + " MAP=" + str(ground_map.get(x, y)))
-			print("O="+str(object_map.get(x, y)))
-			print("O1="+str(object_map.get(x - 1, y)))
-			print("O2="+str(object_map.get(x, y - 1)))
-			print("O3="+str(object_map.get(x - 1, y - 1)))
-			print("C="+str(occupied_spots.find([x, y])))
-			print("W="+str(walkable_tiles.find(ground_map.get(x, y))))
-			pass
 	
 	return [x, y]
 
