5f36d440e76189328ea6c1f1d9cdea989b38f354
nightblade9 <nightbladecodes@gmail.com>
2018-12-16 18:06:43 -0500

    Fix crash bug when save/load on Overworld

--- source/Scripts/SceneManagement.gd
+++ source/Scripts/SceneManagement.gd
@@ -16,7 +16,8 @@ static func change_map_to(tree, target):
 	if typeof(target) != TYPE_STRING:
 		map_type = Globals.current_map.map_type
 		var map_variation = Globals.current_map.variation
-		map_type = map_type + "/" + map_variation
+		if map_variation != null:
+			map_type = map_type + "/" + map_variation
 		
 	var map_data = Globals.maps[map_type]
 	var target_areamap
