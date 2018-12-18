d686c0ff5d9e7afbe933842abe761583970e6f8d
nightblade9 <nightbladecodes@gmail.com>
2018-12-17 23:34:29 -0500

    Equipment balancing. Equipment is always better than what's equipped. Potentially big jumps in stat power.

--- source/Entities/Equipment.gd
+++ source/Entities/Equipment.gd
@@ -2,10 +2,13 @@ extends Node
 
 const StatType = preload("res://Scripts/Enums/StatType.gd")
 
-# Generate items that always are in this range of our stats, eg. weapons that
-# are in the range of 50-75% of our current strength.
-const _MIN_STAT_MULTIPLIER = 0.5
-const _MAX_STAT_MULITPLIER = 0.75
+# Generate items that always are in this range of our equipment,
+# eg. weapons are in the range of 1.1x-1.4x of ours.
+const _MIN_STAT_MULTIPLIER = 1.1
+const _MAX_STAT_MULITPLIER = 1.4
+
+const _MIN_HEALTH_BOOST_PERCENT = 0.1
+const _MAX_HEALTH_BOOST_PERCENT = 0.25
 
 var type # weapon, armour, cloak, etc.
 var equipment_name
@@ -23,8 +26,8 @@ func _init(type, equipment_name, primary_stat, secondary_stat):
 	self.secondary_stat = secondary_stat
 
 func roll_modifiers():
-	self.primary_stat_modifier = _get_random_amount(self.primary_stat)
-	self.secondary_stat_modifier = _get_random_amount(self.secondary_stat)
+	self.primary_stat_modifier = _get_random_amount(self.type, self.primary_stat)
+	self.secondary_stat_modifier = _get_random_amount(self.type, self.secondary_stat)
 
 func to_dict():
 	return ({
@@ -37,15 +40,23 @@ func to_dict():
 		"secondary_stat_modifier": self.secondary_stat_modifier
 	})
 
-static func _get_random_amount(stat_name):
+static func _get_random_amount(type, stat_name):
 	var amount = 0
 	
 	if stat_name == StatType.Health:
 		amount = Globals.player_data.health
-	elif stat_name == StatType.Strength:
-		amount = Globals.player_data.strength
-	elif stat_name == StatType.Defense:
-		amount = Globals.player_data.defense
+		# Not less than 10% health boost, no more than 25%
+		return randint(int(amount * _MIN_HEALTH_BOOST_PERCENT), int(amount * _MAX_HEALTH_BOOST_PERCENT))
+	# Weapon/strength or armour/defense, use primary modifier
+	elif stat_name == StatType.Strength and type == "weapon":
+		amount = Globals.player_data.weapon.primary_stat_modifier
+	elif stat_name == StatType.Defense and type == "armour":
+		amount = Globals.player_data.armour.primary_stat_modifier
+	# Weapon/defense or armour/strength, use secondary modifier
+	elif stat_name == StatType.Defense and type == "weapon":
+		amount = Globals.player_data.weapon.secondary_stat_modifier
+	elif stat_name == StatType.Strength and type == "armour":
+		amount = Globals.player_data.armour.secondary_stat_modifier
 	
 	var min_amount = ceil(amount * _MIN_STAT_MULTIPLIER)
 	var max_amount = ceil(amount * _MAX_STAT_MULITPLIER)
