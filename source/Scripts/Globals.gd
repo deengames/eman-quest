26aac7f3fe83021cb51f91dc86c0960a5df7fc9e
nightblade9 <nightbladecodes@gmail.com>
2018-12-22 17:29:20 -0500

    DRY code to move on mouse-click

--- source/Scripts/Globals.gd
+++ source/Scripts/Globals.gd
@@ -54,6 +54,16 @@ func _ready():
 	# desert dungeon => 123321
 	#seed("abcdef".hash())
 	
+# https://docs.godotengine.org/en/3.0/tutorials/inputs/inputevent.html#how-does-it-work
+# Fires if nothing else handled the event, it seems.
+func _unhandled_input(event):
+	if (event is InputEventMouseButton and event.pressed) or (OS.has_feature("Android") and event is InputEventMouseMotion):
+		var position = get_global_mouse_position()
+		# Clicks seem ... off for some reason. Not sure why. Adjust manually.
+		position.x -= Globals.TILE_WIDTH / 2
+		position.y -= Globals.TILE_HEIGHT
+		Globals.emit_signal("clicked_on_map", position)
+	
 # Returns integer value from min to max inclusive
 # Source: https://godotengine.org/qa/2539/how-would-i-go-about-picking-a-random-number
 func randint(minimum, maximum):
