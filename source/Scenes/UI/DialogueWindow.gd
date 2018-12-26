93ded67359b10fc024e686f312ae84df0f3bd370
nightblade9 <nightbladecodes@gmail.com>
2018-12-25 23:02:52 -0500

    Custom dialog. Looks okay (in-editor).

---
+++ source/Scenes/UI/DialogueWindow.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var speaker_name setget set_speaker_name, get_speaker_name
+var dialogue setget set_dialogue, get_dialogue
+
+func _ready():
+	$Avatar/Sprite.texture = null
+	self.speaker_name = ""
+	self.dialogue = ""
+
+func set_sepaker_name(speaker):
+	$Nametag/NameText.text = speaker
+
+func get_speaker_name():
+	return $Nametag/NameText.text
+	
+func set_dialogue(content):
+	$Panel/DialogueText.text = content
+	
+func get_dialogue():
+	return $Panel/DialogueText.text
