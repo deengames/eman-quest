fcdf516ecf16db5b5ca6f906b0c7e6b58f808dd3
nightblade9 <nightbladecodes@gmail.com>
2018-12-30 21:36:24 -0500

    Fix desert dungeon autotile Draw everything on a single tilemap. Strangely enough, castle dungeon is still broken.

--- source/Scripts/Generators/DungeonGenerator.gd
+++ source/Scripts/Generators/DungeonGenerator.gd
@@ -75,7 +75,6 @@ var map_width = Globals.SUBMAP_WIDTH_IN_TILES
 var map_height = Globals.SUBMAP_HEIGHT_IN_TILES
 
 var _ground_map = []
-var _wall_map = []
 var _rooms = [] # Array of Rect2s
 
 # Called once per game
@@ -85,7 +84,6 @@ func generate(submap, transitions, variation_name):
 
 	var tile_data = self._generate_dungeon(submap.area_type, transitions) # generates paths too
 	self._ground_map = tile_data[0]
-	self._wall_map = tile_data[1]
 
 	map.transitions = transitions
 	map.treasure_chests = self._generate_treasure_chests()
@@ -118,20 +116,29 @@ func _generate_dungeon(area_type, transitions):
 
 	var ground_map = TwoDimensionalArray.new(self.map_width, self.map_height)
 	to_return.append(ground_map)
-	self._fill_with("Ground", ground_map)
-
-	var wall_map = TwoDimensionalArray.new(self.map_width, self.map_height)
-	to_return.append(wall_map)
-	self._fill_with("Ceiling", wall_map)
+	self._fill_with("Ceiling", ground_map)
 	
-	var decoration_map = TwoDimensionalArray.new(self.map_width, self.map_height)
-	to_return.append(decoration_map)
-
-	self._generate_rooms(transitions, ground_map, wall_map, decoration_map)
+	### Super glorious hack for monsters appearing on cave water/lava.
+	# See: DungeonGenerator.gd
+	### This is not going to end well.
+	
+	var solid_tiles_map = TwoDimensionalArray.new(self.map_width, self.map_height)
+	to_return.append(solid_tiles_map)
+	
+	var decoration_tilemap = TwoDimensionalArray.new(self.map_width, self.map_height)
+	to_return.append(decoration_tilemap)
+	
+	self._generate_rooms(transitions, ground_map, decoration_tilemap)
+	
+	for y in range(0, self.map_height):
+		for x in range(0, self.map_width):
+			var tile = ground_map.get(x, y)
+			if tile != "Ground":
+				solid_tiles_map.set(x, y, tile)
 
 	return to_return
 
-func _generate_rooms(transitions, ground_map, wall_map, decoration_map):
+func _generate_rooms(transitions, ground_map, decoration_map):
 	
 	var to_generate = Globals.randint(_NUM_ROOMS[0], _NUM_ROOMS[1])
 	var min_room_distance = _ROOM_SIZE[1] + _PATHS_BUFFER_FROM_EDGE # max size = min distance
@@ -165,7 +172,7 @@ func _generate_rooms(transitions, ground_map, wall_map, decoration_map):
 				continue
 		
 		if generate:
-			self._generate_room(current_room, ground_map, wall_map, decoration_map)
+			self._generate_room(current_room, ground_map, decoration_map)
 			_rooms.append(current_room)
 			if previous != null:
 				var source = _center_of(previous)
@@ -178,7 +185,7 @@ func _generate_rooms(transitions, ground_map, wall_map, decoration_map):
 	for data in paths_to_generate:
 		var source = data[0]
 		var target = data[1]
-		self._generate_straight_path(source, target, ground_map, wall_map, decoration_map)
+		self._generate_straight_path(source, target, ground_map, decoration_map)
 	
 	# Generate a node close to entrances (5-10 tiles "in" from the entrance).
 	# Then, connect entrance => new node => closest path node
@@ -201,16 +208,16 @@ func _generate_rooms(transitions, ground_map, wall_map, decoration_map):
 			# bottom entrance
 			destination[1] -= offset
 		
-		self._generate_straight_path(entrance, destination, ground_map, wall_map, decoration_map)
+		self._generate_straight_path(entrance, destination, ground_map, decoration_map)
 		var closest_node = self._find_closest_room_to(destination, _rooms)
-		self._generate_straight_path(destination, _center_of(closest_node), ground_map, wall_map, decoration_map)
+		self._generate_straight_path(destination, _center_of(closest_node), ground_map, decoration_map)
 
 func _center_of(room):
 	var x = room.position.x + floor(room.size.x / 2)
 	var y = room.position.y + floor(room.size.y / 2)
 	return [x, y]
 
-func _generate_straight_path(point1, point2, ground_map, wall_map, decoration_map):
+func _generate_straight_path(point1, point2, ground_map, decoration_map):
 	var start_x = point1[0]
 	var start_y = point1[1]
 	var stop_x = point2[0]
@@ -221,14 +228,14 @@ func _generate_straight_path(point1, point2, ground_map, wall_map, decoration_ma
 		
 	if randi() % 100 <= 50:
 		# horizontal, then vertical
-		self._generate_path([start_x, start_y], [stop_x, start_y], ground_map, wall_map, decoration_map)
-		self._generate_path([stop_x, start_y], [stop_x, stop_y], ground_map, wall_map, decoration_map)
-		self._generate_door(start_x + ((stop_x - start_x) / 2), start_y, wall_map, decoration_map)
+		self._generate_path([start_x, start_y], [stop_x, start_y], ground_map, decoration_map)
+		self._generate_path([stop_x, start_y], [stop_x, stop_y], ground_map, decoration_map)
+		self._generate_door(start_x + ((stop_x - start_x) / 2), start_y, ground_map, decoration_map)
 	else:
 		# vertical, then horizontal
-		self._generate_path([start_x, start_y], [start_x, stop_y], ground_map, wall_map, decoration_map)
-		self._generate_path([start_x, stop_y], [stop_x, stop_y], ground_map, wall_map, decoration_map)
-		self._generate_door(start_x, start_y + ((stop_y - start_y) / 2), wall_map, decoration_map)
+		self._generate_path([start_x, start_y], [start_x, stop_y], ground_map, decoration_map)
+		self._generate_path([start_x, stop_y], [stop_x, stop_y], ground_map, decoration_map)
+		self._generate_door(start_x, start_y + ((stop_y - start_y) / 2), ground_map, decoration_map)
 
 func _find_closest_room_to(room, rooms):
 	var closest_node = rooms[0]
@@ -242,38 +249,38 @@ func _find_closest_room_to(room, rooms):
 	
 	return closest_node
 
-func _generate_door(x, y, wall_map, decoration_map):
+func _generate_door(x, y, ground_map, decoration_map):
 	var adjacent_ground = 0
 	
-	if wall_map.get(x - 1, y - 1) == null:
+	if ground_map.get(x - 1, y - 1) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x, y - 1) == null:
+	if ground_map.get(x, y - 1) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x + 1, y - 1) == null:
+	if ground_map.get(x + 1, y - 1) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x - 1, y) == null:
+	if ground_map.get(x - 1, y) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x + 1, y) == null:
+	if ground_map.get(x + 1, y) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x - 1, y + 1) == null:
+	if ground_map.get(x - 1, y + 1) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x, y + 1) == null:
+	if ground_map.get(x, y + 1) == "Ground":
 		adjacent_ground += 1
-	if wall_map.get(x + 1, y + 1) == null:
+	if ground_map.get(x + 1, y + 1) == "Ground":
 		adjacent_ground += 1
 	
 	# Must have two adjacent wall tiles
 	if adjacent_ground == 2:
 		self._set_tile([x, y], "Door", decoration_map)
 
-func _generate_room(room_rect, ground_map, wall_map, decoration_map):
+func _generate_room(room_rect, ground_map, decoration_map):
 	var wall_tiles = _ROOM_WALL_HEIGHT - _NUM_CEILING_TILES
 	
 	for v in range(room_rect.size.y):
 		for u in range(room_rect.size.x):
 			var x = room_rect.position.x + u
 			var y = room_rect.position.y + v
-			self._convert_to_ground([x, y], ground_map, wall_map, decoration_map)
+			self._convert_to_ground([x, y], ground_map, decoration_map)
 	
 	for v in range(0, _ROOM_WALL_HEIGHT): # five tiles tall
 		for u in range(room_rect.size.x):
@@ -283,27 +290,27 @@ func _generate_room(room_rect, ground_map, wall_map, decoration_map):
 			# Three rows ceiling, then two of wall
 			if v < _NUM_CEILING_TILES:
 				type = "Ceiling"
-			self._set_tile([x, y], type, wall_map)
+			self._set_tile([x, y], type, ground_map)
 	
 	var num_torches = randi() % (_MAX_TORCHES_PER_ROOM + 1)
 	var x_per_torch = round(room_rect.size.x / (num_torches + 1))
 	
 	for i in range(num_torches):
-		self._add_torch(room_rect.position.x + ((i + 1) * x_per_torch), room_rect.position.y - 1, wall_map, decoration_map)
+		self._add_torch(room_rect.position.x + ((i + 1) * x_per_torch), room_rect.position.y - 1, ground_map, decoration_map)
 	
-func _add_torch(x, y, wall_map, decoration_map):
+func _add_torch(x, y, ground_map, decoration_map):
 	# Place only on walls
-	if wall_map.get(x, y) == "Wall":
+	if ground_map.get(x, y) == "Wall":
 		decoration_map.set(x, y, "Torch")
 
-func _generate_path(point1, point2, ground_map, wall_map, decoration_map):
+func _generate_path(point1, point2, ground_map, decoration_map):
 	var from_x = point1[0]
 	var from_y = point1[1]
 
 	var to_x = point2[0]
 	var to_y = point2[1]
 
-	self._convert_to_ground([from_x, from_y], ground_map, wall_map, decoration_map)
+	self._convert_to_ground([from_x, from_y], ground_map, decoration_map)
 
 	while from_x != to_x or from_y != to_y:
 		# If we're farther away on x-axis, move horizontally.
@@ -312,14 +319,14 @@ func _generate_path(point1, point2, ground_map, wall_map, decoration_map):
 		else:
 			from_y += sign(to_y - from_y)
 
-		self._convert_to_ground([from_x, from_y], ground_map, wall_map, decoration_map)
+		self._convert_to_ground([from_x, from_y], ground_map, decoration_map)
 		
 		# Decorate any paths with 2-tile high walls
 		# This shortens ceilings and shows awesome walls instead
-		if wall_map.get(from_x, from_y - 1) == "Ceiling":
-			self._set_tile([from_x, from_y - 1], "Wall", wall_map)
-		if wall_map.get(from_x, from_y - 2) == "Ceiling":
-			self._set_tile([from_x, from_y - 2], "Wall", wall_map)
+		if ground_map.get(from_x, from_y - 1) == "Ceiling":
+			self._set_tile([from_x, from_y - 1], "Wall", ground_map)
+		if ground_map.get(from_x, from_y - 2) == "Ceiling":
+			self._set_tile([from_x, from_y - 2], "Wall", ground_map)
 
 func _generate_treasure_chests():
 	var num_chests = Globals.randint(_NUM_CHESTS[0], _NUM_CHESTS[1])
@@ -328,9 +335,14 @@ func _generate_treasure_chests():
 	var types = ["weapon", "armour"]
 	var stats = {"weapon": StatType.Strength, "armour": StatType.Defense}
 
+	######## duplicated in cave generator
+	# Sigh, refactoring. This map has no separate object/wall map. So pass
+	# an empty map in here.
+	var empty_map = TwoDimensionalArray.new(map_width, map_height)
+	
 	while num_chests > 0:
 		var spot = SpotFinder.find_empty_spot(map_width, map_height,
-			self._ground_map, self._wall_map, ["Ground"], chests_coordinates)
+			self._ground_map, empty_map, ["Ground"], chests_coordinates)
 
 		var type = types[randi() % len(types)]
 		var stat = stats[type]
@@ -349,10 +361,10 @@ func _fill_with(tile_name, map_array):
 		for x in range(0, map_width):
 			map_array.set(x, y, tile_name)
 
-func _convert_to_ground(position, ground_map, wall_map, decoration_map):
-	self._convert_to(position, "Ground", ground_map, wall_map, decoration_map)
+func _convert_to_ground(position, ground_map, decoration_map):
+	self._convert_to(position, "Ground", ground_map, decoration_map)
 
-func _convert_to(position, type, ground_map, wall_map, decoration_map):
+func _convert_to(position, type, ground_map, decoration_map):
 	# Draws dirt at the specified position. Also clears trees for
 	# one tile in all directions surrounding the dirt (kind of like
 	# drawing with a 3x3 grass brush around the dirt).
@@ -361,7 +373,6 @@ func _convert_to(position, type, ground_map, wall_map, decoration_map):
 
 	if x >= 0 and x < map_width and y >= 0 and y < map_height:
 		ground_map.set(x, y, type)
-		wall_map.set(x, y, null) # remove wall
 		decoration_map.set(x, y, null) # remove decoration
 
 func _set_tile(position, type, map):
@@ -374,9 +385,9 @@ func _set_tile(position, type, map):
 	if x >= 0 and x < map_width and y >= 0 and y < map_height:
 		map.set(x, y, type)
 
-func _clear_if_wall(wall_map, x, y):
+func _clear_if_wall(ground_map, x, y):
 	if x >= 0 and x < map_width:
 		if y >= 0 and y < map_height:
-			if wall_map.get(x, y) != null:
-				wall_map.set(x, y, null)
+			if ground_map.get(x, y) == "Wall" or ground_map.get(x, y) == "Ceiling":
+				ground_map.set(x, y, null)
 
