43cea267196ecaeba062be1991592a83d184786b
nightblade9 <nightbladecodes@gmail.com>
2019-01-19 00:12:36 -0500

    Fixed wierd crash on end-game map Fix camera not moving (moved all tiles)

--- source/Scenes/Maps/EndGameMap.gd
+++ source/Scenes/Maps/EndGameMap.gd
@@ -1,13 +1,10 @@
 extends "StaticMap.gd"
 
-const Player = preload("res://Entities/Player.tscn")
-
 const map_type = "Final"
 
 func _ready():
 	var player = Globals.player
 	player.position = $Locations/Entrance.position
-	self.add_child(player)
 	
 	if Globals.bosses_defeated >= 3:
 		$Umayyah.visible = true
