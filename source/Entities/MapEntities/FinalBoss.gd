9a4791e272d4719333cf7f97df1a1f193f3bc470
nightblade9 <nightbladecodes@gmail.com>
2019-01-20 21:57:59 -0500

    Experiment with whiting-out Umayyah

--- source/Entities/MapEntities/FinalBoss.gd
+++ source/Entities/MapEntities/FinalBoss.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
-func face_down():
+const AlphaFluctuator = preload("res://Scripts/Effects/AlphaFluctuator.gd")
+
+func _ready():
 	$Sprite.visible = false
+	$HumanSpriteUp.visible = false
+	$HumanSpriteDown.visible = false
+	$WhiteOut.visible = false
+	
+func face_down():
 	$HumanSpriteUp.visible = false
 	$HumanSpriteDown.visible = true
 	
 func face_up():
-	$Sprite.visible = false
 	$HumanSpriteUp.visible = true
 	$HumanSpriteDown.visible = false
+
+func flicker():
+	$WhiteOut.visible = true
+	$WhiteOut.modulate.a = 0
+	
+	var fluc = AlphaFluctuator.new($WhiteOut)
+	self.add_child(fluc)
+	fluc.run(3)
