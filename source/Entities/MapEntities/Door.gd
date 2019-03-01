67b50aaadd75fc1aeba3290459d257da55b8335a
nightblade9 <nightbladecodes@gmail.com>
2019-02-28 19:46:38 -0500

    Fix: single-wall desert tiles shouldn't be walkable

--- source/Entities/MapEntities/Door.gd
+++ source/Entities/MapEntities/Door.gd
@@ -5,4 +5,6 @@ func _on_Area2D_body_entered(body):
 		# TODO: play a click-click noise
 		# NB: shared between regular and desert door. Parameterize if should differ.
 		self.queue_free()
+		# TODO: emit signal and remove on parent. Throws an error at runtime:
+		# "This function can't be used during the in/out signal."
 		self.get_parent().remove_child(self)
