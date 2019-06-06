18e2efb0f74f928aba36393a934851e7c2334b6a
nightblade9 <nightbladecodes@gmail.com>
2019-06-06 16:19:59 -0400

    Tutorial arrow, now with clicky circle.

--- source/Scenes/UI/ClickHereArrow.gd
+++ source/Scenes/UI/ClickHereArrow.gd
@@ -6,9 +6,9 @@ var _total_seconds = 0
 var _base_y = 0
 
 func _ready():
-	_base_y = position.y
+	_base_y = $Arrow.position.y
 
 func _process(delta):
 	_total_seconds += delta
 	# Maps 1 ... -1 to 0 ... 2
-	self.position.y = _base_y + (cos(_total_seconds * _SPEED_MULTIPLIER) - 1) * _WAVE_HEIGHT
+	$Arrow.position.y = _base_y + (cos(_total_seconds * _SPEED_MULTIPLIER) - 1) * _WAVE_HEIGHT
