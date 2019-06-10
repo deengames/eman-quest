40055a6ffc5e9653738ec095610521f272f31255
nightblade9 <nightbladecodes@gmail.com>
2019-06-09 21:34:33 -0400

    Second cut of save game Now saves a screenshot (with buttons)

--- source/Scenes/UI.gd
+++ source/Scenes/UI.gd
@@ -23,3 +23,27 @@ func _show_popup(instance):
 
 func _on_SaveButton_pressed():
 	SaveManager.save("test")
+	_capture_screenshot()
+	
+func _capture_screenshot():
+	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Let two frames pass to make sure the screen was captured
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	# Retrieve the captured image
+	var img = get_viewport().get_texture().get_data()
+  
+	# Flip it on the y-axis (because it's flipped)
+	img.flip_y()
+
+	# Create a texture for it
+	var tex = ImageTexture.new()
+	tex.create_from_image(img)
+	
+	var capture = TextureRect.new()
+
+	# Set it to the capture node
+	capture.set_texture(tex)
+	
+	img.save_png("user://screenshot-test.png")
--- source/Scenes/UI/SaveSelectWindow.gd
+++ source/Scenes/UI/SaveSelectWindow.gd
@@ -6,3 +6,7 @@ func _ready():
 	for i in range(_NUM_SAVES):
 		var n = i + 1
 		$HBoxContainer/Container/ItemList.add_item("File " + str(n))
+
+func _on_ItemList_item_selected(index):
+	# get data blob based on `index`
+	$HBoxContainer/Container2/SaveDetailsPanel/StatsLabel.text = "World: #123456789\nPlay time: 0:30:13\nLevel: 7"
