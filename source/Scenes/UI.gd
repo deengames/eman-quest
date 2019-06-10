4af65dc55904b1ab6eb2cbdf238aae9d3e158cef
nightblade9 <nightbladecodes@gmail.com>
2019-06-09 23:21:18 -0400

    Show/hide UI on screenshot save

--- source/Scenes/UI.gd
+++ source/Scenes/UI.gd
@@ -23,27 +23,47 @@ func _show_popup(instance):
 
 func _on_SaveButton_pressed():
 	SaveManager.save("test")
+	
+	_hide_ui()
+	
+	# Need some sort of delay before we save, or they still appear
+	# idle_frame itself is not enough; we need two of these before
+	# the buttons actually disappear.
+	for i in range(2):
+		yield(get_tree(), "idle_frame")
+	
 	_capture_screenshot()
+	_show_ui()
+	
+func _hide_ui():
+	# CanvasLayer doesn't have an easy show/hide
+	var ui_layer = get_parent().get_node("UI")
+	for button in ui_layer.get_children():
+		button.hide()
+
+func _show_ui():
+	# CanvasLayer doesn't have an easy show/hide
+	var ui_layer = get_parent().get_node("UI")
+	for button in ui_layer.get_children():
+		button.show()
 	
 func _capture_screenshot():
 	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
-	# Let two frames pass to make sure the screen was captured
-	yield(get_tree(), "idle_frame")
-	yield(get_tree(), "idle_frame")
-
+	
 	# Retrieve the captured image
-	var img = get_viewport().get_texture().get_data()
+	var image = get_viewport().get_texture().get_data()
   
 	# Flip it on the y-axis (because it's flipped)
-	img.flip_y()
+	image.flip_y()
 
 	# Create a texture for it
-	var tex = ImageTexture.new()
-	tex.create_from_image(img)
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
 	
+	# Use a TextureReact to capture data to `texture`
 	var capture = TextureRect.new()
 
 	# Set it to the capture node
-	capture.set_texture(tex)
+	capture.set_texture(texture)
 	
-	img.save_png("user://screenshot-test.png")
+	image.save_png("user://screenshot-test.png")
