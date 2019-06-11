dcddf83388b64dae03b9f6c7c889788e27999f4e
nightblade9 <nightbladecodes@gmail.com>
2019-06-10 21:37:59 -0400

    Remove/clean load screenshot code

--- source/Scenes/UI/SaveSelectWindow.gd
+++ source/Scenes/UI/SaveSelectWindow.gd
@@ -12,22 +12,16 @@ func _on_ItemList_item_selected(index):
 	#$HBoxContainer/Container2/SaveDetailsPanel/StatsLabel.text = "World: #123456789\nPlay time: 0:30:13\nLevel: 7"
 	
 	var sprite = $HBoxContainer/Container2/SaveDetailsPanel/ScreenshotSprite
-	var image = Image.new()
 	
 	var file = File.new()
 	file.open("user://screenshot-test.png", File.READ)
 	var buffer = file.get_buffer(file.get_len())
 	file.close()
 	
+	var image = Image.new()
 	image.load_png_from_buffer(buffer)
+	
 	var image_texture = ImageTexture.new()
 	image_texture.create_from_image(image)
+	
 	sprite.texture = image_texture
-	
-	var data = image.get_data()
-	print(str(data))
-	var size = image.get_size()
-	print(str(size))
-	
-	#sprite.texture = load("user://screenshot-test.png")
-	pass
