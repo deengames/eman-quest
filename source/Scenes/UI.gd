f98142c0ca91edd1162cf083a0ab238391657cbd
nightblade9 <nightbladecodes@gmail.com>
2019-06-10 21:37:52 -0400

    Simplify save-screenshot code

--- source/Scenes/UI.gd
+++ source/Scenes/UI.gd
@@ -23,47 +23,13 @@ func _show_popup(instance):
 
 func _on_SaveButton_pressed():
 	SaveManager.save("test")
-	
-	_hide_ui()
-	
-	# Need some sort of delay before we save, or they still appear
-	# idle_frame itself is not enough; we need two of these before
-	# the buttons actually disappear.
-	for i in range(2):
-		yield(get_tree(), "idle_frame")
-	
 	_capture_screenshot()
-	_show_ui()
-	
-func _hide_ui():
-	# CanvasLayer doesn't have an easy show/hide
-	var ui_layer = get_parent().get_node("UI")
-	for button in ui_layer.get_children():
-		button.hide()
-
-func _show_ui():
-	# CanvasLayer doesn't have an easy show/hide
-	var ui_layer = get_parent().get_node("UI")
-	for button in ui_layer.get_children():
-		button.show()
 	
 func _capture_screenshot():
-	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
-	
 	# Retrieve the captured image
 	var image = get_viewport().get_texture().get_data()
-  
+	
 	# Flip it on the y-axis (because it's flipped)
 	image.flip_y()
-
-	# Create a texture for it
-	var texture = ImageTexture.new()
-	texture.create_from_image(image)
-	
-	# Use a TextureReact to capture data to `texture`
-	var capture = TextureRect.new()
-
-	# Set it to the capture node
-	capture.set_texture(texture)
 	
 	image.save_png("user://screenshot-test.png")
