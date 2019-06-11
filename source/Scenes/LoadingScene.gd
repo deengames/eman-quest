edba2031ecb2d4d4c6fa5ad2d95d9848ce2e826b
nightblade9 <nightbladecodes@gmail.com>
2019-06-10 22:37:48 -0400

    Load button now gives save dialog You can actually load a game!

--- source/Scenes/LoadingScene.gd
+++ source/Scenes/LoadingScene.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
 var SaveManager = preload("res://Scripts/SaveManager.gd")
+var SaveSelectWindow = preload("res://Scenes/UI/SaveSelectWindow.tscn")
 
 func _ready():
+	var window = SaveSelectWindow.instance()
+	window.disable_saving()
+	add_child(window)
+	window.popup_centered()
+	
+	window.connect("on_load", self, "_load_game")
+	
+func _load_game(save_id):
 	# Wait just long enough for the scene to display, then generate
 	yield(get_tree().create_timer(0.25), 'timeout')
-	SaveManager.load("test", get_tree())
+	SaveManager.load("save" + str(save_id), get_tree())
--- source/Scenes/UI/SaveSelectWindow.gd
+++ source/Scenes/UI/SaveSelectWindow.gd
@@ -2,21 +2,33 @@ extends WindowDialog
 
 const SaveManager = preload("res://Scripts/SaveManager.gd")
 
+signal on_load
+
 var _selected_slot = null
+var _save_disabled = false # for titlescreen only
 
 func _ready():
 	for i in range(Globals.NUM_SAVES):
 		var n = i + 1
 		$HBoxContainer/Container/ItemList.add_item("File " + str(n))
 
+func disable_saving():
+	_save_disabled = true
+	$HBoxContainer/Container2/SaveDetailsPanel/VBoxContainer/SaveButton.hide()
+
 func _on_ItemList_item_selected(index):
 	_selected_slot = index
+
 	var label = $HBoxContainer/Container2/SaveDetailsPanel/StatsLabel
+	var sprite = $HBoxContainer/Container2/SaveDetailsPanel/ScreenshotSprite
+	var save_exists = SaveManager.save_exists("save" + str(index))
 	
-	if SaveManager.save_exists("save" + str(index)):
+	$HBoxContainer/Container2/SaveDetailsPanel/VBoxContainer/SaveButton.disabled = not save_exists
+	$HBoxContainer/Container2/SaveDetailsPanel/VBoxContainer/LoadButton.disabled = not save_exists
+	
+	if save_exists:
 		# get data blob based on `index`
 		label.text = "World: #123456789\nPlay time: 0:30:13\nLevel: 7"
-		var sprite = $HBoxContainer/Container2/SaveDetailsPanel/ScreenshotSprite
 		
 		var file = File.new()
 		file.open(_screenshot_path(index), File.READ)
@@ -32,9 +44,10 @@ func _on_ItemList_item_selected(index):
 		sprite.texture = image_texture
 	else:
 		label.text = "Empty"
+		sprite.texture = null
 		
 func _on_SaveButton_pressed():
-	if _selected_slot != null:
+	if not _save_disabled and _selected_slot != null:
 		SaveManager.save("save" + str(_selected_slot))
 		
 		# Copy screenshot from last-saved to this slot
@@ -52,3 +65,7 @@ func _on_SaveButton_pressed():
 		
 func _screenshot_path(save_id):
 	return "user://screenshot-save" + str(save_id) + ".png"
+
+func _on_LoadButton_pressed():
+	if _selected_slot != null:
+		emit_signal("on_load", _selected_slot)
