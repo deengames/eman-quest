97d276afe36640752b30abb8cd451ec4d657a18f
nightblade9 <nightbladecodes@gmail.com>
2019-06-11 20:57:44 -0400

    Castle and desert dungeon audio. All CC0.

--- source/Scripts/AudioManager.gd
+++ source/Scripts/AudioManager.gd
@@ -16,7 +16,9 @@ var audio_clips = {
 	"frost-forest-bgs": preload("res://assets/audio/bgs/Frost-Forest.ogg"),
 	"death-forest-bgs": preload("res://assets/audio/bgs/Death-Forest.ogg"),
 	"river-cave-bgs": preload("res://assets/audio/bgs/River-Cave.ogg"),
-	"lava-cave-bgs": preload("res://assets/audio/bgs/Lava-Cave.ogg")
+	"lava-cave-bgs": preload("res://assets/audio/bgs/Lava-Cave.ogg"),
+	"castle-dungeon-bgs": preload("res://assets/audio/bgs/Castle-Dungeon.ogg"),
+	"desert-dungeon-bgs": preload("res://assets/audio/bgs/Desert-Dungeon.ogg")
 }
 
 const AudioFilePlayerClass = preload("res://Scenes/AudioFilePlayer.tscn")
