ba38849e1de38cd65e4c1d9c379c8cf9d21e0d2c
nightblade9 <nightbladecodes@gmail.com>
2019-06-16 06:05:39 -0400

    Rework credits to be more accessibility friendly Added bug font image, fade-in instead of moving Added game time to it, refactored out of save select screen to player data

--- source/Scenes/UI/SaveSelectWindow.gd
+++ source/Scenes/UI/SaveSelectWindow.gd
@@ -1,6 +1,7 @@
 extends WindowDialog
 
 const AudioManager = preload("res://Scripts/AudioManager.gd")
+const PlayerData = preload("res://Entities/PlayerData.gd")
 const SaveManager = preload("res://Scripts/SaveManager.gd")
 const SceneFadeManager = preload("res://Scripts/Effects/SceneFadeManager.gd")
 
@@ -49,7 +50,7 @@ func _on_ItemList_item_selected(index):
 		label.text = "World: #{seed}\nPlay time: {play_time}\nLevel: {level}" \
 			.format({
 				"seed": str(data["seed_value"]),
-				"play_time": _seconds_to_time(data["player_data"].play_time_seconds),
+				"play_time": PlayerData.seconds_to_time(data["player_data"].play_time_seconds),
 				"level": int(data["player_data"].level)
 			})
 		sprite.texture = _get_screenshot_for(index)
@@ -102,31 +103,3 @@ func _on_LoadButton_pressed():
 		# disappear without triggering popup_hide, which takes us to the titlescreen
 		self.modulate.a = 0 
 		SaveManager.load("save" + str(_selected_slot), get_tree())
-
-func _seconds_to_time(total_seconds):
-	var seconds = int(total_seconds)
-	var display_seconds = seconds % 60
-	var display_minutes = int(seconds / 60)
-	var display_hours = int(display_minutes / 60)
-	
-	var final_minutes = display_minutes
-	if display_hours > 0:
-		final_minutes = _two_digit(display_minutes)
-	
-	if display_hours > 0:
-		return "{h}:{m}:{s}".format({
-			"h": display_hours,
-			"m": final_minutes,
-			"s": _two_digit(display_seconds)
-		})
-	else:
-		return "{m}:{s}".format({
-			"m": final_minutes,
-			"s": _two_digit(display_seconds)
-		})
-
-func _two_digit(n):
-	if n <= 9:
-		return "0" + str(n)
-	else:
-		return str(n)
