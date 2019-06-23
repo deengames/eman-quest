e60fb9e3b93d254525b26d9453e42b8035574f92
nightblade9 <nightbladecodes@gmail.com>
2019-06-23 16:33:10 -0400

    Travis: fix wget 404 failure

--- source/run_tests.sh
+++
@@ -1,3 +0,0 @@
-wget "https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_linux_headless.64.zip"
-chmod +x Godot_v3.0.6-stable_linux_headless.64
-Godot_v3.0.6-stable_linux_headless.64 -d -s --path . addons\gut\gut_cmdln.gd
