e2302724e304f877777da21c078c68a45e37865a
nightblade9 <nightbladecodes@gmail.com>
2019-06-23 16:13:49 -0400

    Scripts and config to run GUT tests through the cmdline

---
+++ source/run_tests.sh
@@ -0,0 +1,3 @@
+wget https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_linux_headless.64.zip
+chmod +x Godot_v3.0.6-stable_linux_headless.64
+Godot_v3.0.6-stable_linux_headless.64 -d -s --path . addons\gut\gut_cmdln.gd
