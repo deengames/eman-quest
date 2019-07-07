a4bb06c3e5837c0947330c8e3fd1c6259131ea1d
nightblade9 <nightbladecodes@gmail.com>
2019-07-07 15:45:53 -0400

    Splash: fade works

--- source/Scenes/DeenGamesSplash.gd
+++ source/Scenes/DeenGamesSplash.gd
@@ -5,29 +5,25 @@ const _SHOW_SECONDS = 3
 
 func _ready():
 	var tree = get_tree()
-	var root = tree.get_root()
 	
 	$Logo.modulate = Color(1, 1, 1, 0)
 	
-	print("Start")
-	print("In")
+	# Tween starts before display renders, so add a bit of delay before we start
+	yield(tree.create_timer(0.5), 'timeout')
+	
 	# Fade in
 	var tween = Tween.new()
 	tween.interpolate_property($Logo, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), _FADE_SECONDS, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	root.add_child(tween)
+	add_child(tween)
 	tween.start()
 	yield(tree.create_timer(_FADE_SECONDS), 'timeout')
 	
-	print("Chill")
 	# Chill
 	yield(tree.create_timer(_SHOW_SECONDS), 'timeout')
 	
 	# Fade out
-	print("Out")
 	tween = Tween.new()
 	tween.interpolate_property($Logo, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), _FADE_SECONDS, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	root.add_child(tween)
+	add_child(tween)
 	tween.start()
 	yield(tree.create_timer(_FADE_SECONDS), 'timeout')
-	
-	print("Done")
