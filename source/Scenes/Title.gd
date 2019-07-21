dd60705f386b091d223d0bc8075f1bfbb77d4844
nightblade9 <nightbladecodes@gmail.com>
2019-07-20 21:37:21 -0400

    Options correctly apply on splash screen Fix bug where audio is still audible at 0 (-50dB)

--- source/Scenes/Title.gd
+++ source/Scenes/Title.gd
@@ -7,34 +7,10 @@ const OptionsSaver = preload("res://Scripts/OptionsSaver.gd")
 const SceneFadeManager = preload("res://Scripts/Effects/SceneFadeManager.gd")
 const SceneManagement = preload("res://Scripts/SceneManagement.gd")
 
-var _GAME_WIDTH = ProjectSettings.get_setting("display/window/size/width")
-var _GAME_HEIGHT = ProjectSettings.get_setting("display/window/size/height")
-
 var _audio
 
 func _ready():
 	var data = OptionsSaver.load()
-	
-	if data == null:
-		data = {
-			"zoom": 100,
-			"monsters_chase": Features.is_enabled("monsters chase you"),
-			"is_first_run": true,
-			"is_full_screen": true,
-			# ranges from -40 (muted) to 0 (full volume)
-			"background_volume": 0,
-			"sfx_volume": 0
-		}
-	
-	var window_size = OS.window_size
-	Globals.zoom  = data["zoom"]
-	var zoom_percent = Globals.zoom / 100
-	OS.window_size = Vector2(_GAME_WIDTH * zoom_percent, _GAME_HEIGHT * zoom_percent)
-	
-	Globals.background_volume = data["background_volume"]
-	Globals.sfx_volume = data["sfx_volume"]
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background"), Globals.background_volume)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), Globals.sfx_volume)
 	
 	Features.set_state("monsters chase you", data["monsters_chase"])
 	Globals.is_full_screen = data["is_full_screen"]
