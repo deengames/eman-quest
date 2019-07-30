a4966f44950f82dd42fb1cd3e9d5ccf90be8dbbe
nightblade9 <nightbladecodes@gmail.com>
2019-07-29 23:24:51 -0400

    Revert "Fix infrequent crash after losing a battle" This change broke loading: bosses/monsters never reappear.
    This reverts commit 34f9466d616036d797e33501a024389dcb8ddfd3.

--- source/Scripts/DictionaryHelper.gd
+++ source/Scripts/DictionaryHelper.gd
@@ -1,7 +1,5 @@
 extends Node
 
-const ReferenceChecker = preload("res://Scripts/ReferenceChecker.gd")
-
 # Given an array of entities, return an array where every item is created by
 # calling to_dict on the items in the input array.
 static func array_to_dictionary(array):
@@ -31,8 +29,7 @@ static func dictionary_values_to_dictionary(dict):
 		var a = []
 		
 		for item in dict[key]:
-			if not ReferenceChecker.is_previously_freed(item) and "to_dict" in item:
-				a.append(item.to_dict())
+			a.append(item.to_dict())
 			
 		to_return[key] = a
 	
